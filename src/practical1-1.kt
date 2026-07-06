e983e06ae5f308bdf7b4c13f4af205bc8d4c678b
Dhruvi-patel05 <dpatel040906@gmail.com>
2026-07-06 18:22:19 +0530

    PRACTICAL

---
+++ src/practical1-1.kt
@@ -0,0 +1,22 @@
+fun main(){
+    val n: Int =10
+    val a: Float = 2.5f
+    val c: Char = 'D'
+    val s: String ="Patel"
+    val d: Boolean = true
+    val b: Double = 96.36
+    val e: Long = 1234567890
+    val h: Short = -6
+    val i: Byte = 127
+
+    println("Store & Display Values in Different Variables: ")
+    println("n=" +n)
+    println("a=" +a)
+    println("c=" +c)
+    println("s=" +s)
+    println("d=" +d)
+    println("b=" +b)
+    println("e=" +e)
+    println("h=" +h)
+    println("i=" +i)
+}
